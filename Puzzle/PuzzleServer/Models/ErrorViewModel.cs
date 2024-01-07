c63442fbf5eb5279b1bd6af98e345a4f1a4811ad
TemporaryAccountGithub <tmpanonimi@gmail.com>
2024-01-07 15:38:51 +0200

    removing uneeded files

--- Puzzle/PuzzleServer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PuzzleServer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
